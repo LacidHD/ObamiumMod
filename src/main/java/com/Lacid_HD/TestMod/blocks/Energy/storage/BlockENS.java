78dfdc9eef7c34a68d360d8d2d586b3e68b9919c
Lasse <Lasse.Maghon@gmail.com>
2020-09-23 17:39:46 +0200

    help

---
+++ src/main/java/com/Lacid_HD/TestMod/blocks/Energy/storage/BlockENS.java
@@ -0,0 +1,5 @@
+package com.Lacid_HD.TestMod.blocks.Energy.storage;
+
+public class BlockENS {
+
+}
--- src/main/java/com/Lacid_HD/TestMod/init/ModBlocks.java
+++ src/main/java/com/Lacid_HD/TestMod/init/ModBlocks.java
@@ -9,6 +9,7 @@ import com.Lacid_HD.TestMod.blocks.MemeOre;
 import com.Lacid_HD.TestMod.blocks.ObamiumBlock;
 import com.Lacid_HD.TestMod.blocks.ObamiumOre;
 //import com.Lacid_HD.TestMod.blocks.machines.refinery.BlockRefinery;
+import com.Lacid_HD.TestMod.blocks.Energy.storage.BlockENS;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -31,5 +32,10 @@ public class ModBlocks
 	
 	//machines
 	//public static final Block REFINERY = new BlockRefinery("refinery");
+	
+	
+	
+	//energy
+	//public static final Block ENS_1 = new BlockENS("ens_1");
 	
 }	
